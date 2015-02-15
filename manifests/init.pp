0e852ead3cd58435c8660b31f557a284ee75bcdf
Patrik Majer <patrik.majer@atlas.cz>
2015-02-15 21:34:48 +0100

    switch to module structure

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,21 +8,15 @@
 #							#
 #########################################################
 
-import 'own-cloud-system.pp'
+import 'system.pp'
 
-class { '::mysql::server':
+class { 'mysql::server':
   override_options => {
     'mysqld' => { 'bind-address' => '127.0.0.1' }
   },
 }
 
 class { 'owncloud': 
-#  db_user => 'owncloud',
-#  db_pass => 'nqrEtRid',
-
-  ssl      => true,
-  ssl_cert => '/etc/apache2/ssl/owncloud.hujer.info.crt',
-  ssl_key  => '/etc/apache2/ssl/owncloud.hujer.info.key'
 }
 
-import 'own-cloud-accessories.pp'
+import 'accessories.pp'
