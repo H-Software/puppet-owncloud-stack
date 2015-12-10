5975627e7da313f88ce4349aa697c51b9f0ec36f
Patrik Majer <patrik.majer@atlas.cz>
2015-12-10 17:07:34 +0100

    fix module syntax

---
+++ manifests/services.pp
@@ -0,0 +1,84 @@
+#########################################################
+#							#
+# OwnCLoud manifest - accesories part			#
+#							#
+# create by czhujer (patrik.majer.pisek@gmail.com) 	#
+#							#
+# version: 0.3						#
+#							#
+#########################################################
+
+owncloudstack::services ()
+{
+
+  #
+  # Antivirus part
+  #
+
+  if($::operatingsystem == "centos" and $::operatingsystemrelease >= 6 and $::operatingsystemrelease < 7) {
+    $packages_clamav = ["clamav", "clamd", "clamav-db"]
+    $service_clamav = "clamd"
+  }
+  else{
+    $packages_clamav = ["clamav", "clamav-base", "clamav-daemon", "clamav-freshclam"]
+    $service_clamav = "clamav-freshclam"
+  }
+
+  package { $packages_clamav:
+      ensure => latest,
+  }
+
+  if($::operatingsystem == "ubuntu") {
+
+    service { 'clamav-freshclam':
+      ensure => running,
+      enable => true,
+      require => Package[$packages_clamav],
+    }
+
+  }
+
+  service { 'clamav-daemon':
+    name   => $service_clamav,
+    ensure => running,
+    enable => true,
+    require => Package[$packages_clamav],
+  }
+
+  #
+  # Cron part
+  #
+
+  if($::operatingsystem == "centos" and $::operatingsystemrelease >= 6 and $::operatingsystemrelease < 7) {
+    $package_name = 'cronie'
+    $service_name = 'crond'
+  }
+  else
+  {
+    $package_name = 'cron' 
+    $service_name = 'cron'
+  }
+
+  package {'cron':
+      ensure      => 'present',
+      name        => $package_name,
+  }
+
+  service {'cron':
+    enable      => "true",                     # true (start on boot)
+    ensure      => "true",                     # true (running), false (stopped)
+    name        => $service_name,
+    require     => Package['cron'],
+    subscribe	=> File['owcnloud cron file'],
+  }
+
+  $cron_file_owncloud = "*/15  *  *  *  * www-data php -f /var/www/owncloud/cron.php > /dev/null 2>&1
+  "
+
+  file { 'owcnloud cron file':
+    name => '/etc/cron.d/owncloud',
+    content => $cron_file_owncloud,
+    require => Package["cron"],
+  }
+
+}
--- manifests/system.pp
+++ manifests/system.pp
@@ -4,54 +4,57 @@
 #                                                       #
 # create by czhujer (patrik.majer.pisek@gmail.com)      #
 #                                                       #
-# version: 0.2                                          #
+# version: 0.3                                          #
 #                                                       #
 #########################################################
 
-include ntp
+class owncloudstack::system ()
+{
 
-include timezone
+  include ntp
+
+  include timezone
 
 
-if($::operatingsystem == "ubuntu") {
+  if($::operatingsystem == "ubuntu") {
 
-  class { 'fail2ban':
-    package_ensure => 'latest',
-    config_file_template => "fail2ban/${::lsbdistcodename}/etc/fail2ban/jail.conf.erb",
-    bantime => 3600,
-    require => Package['sendmail'],
+    class { 'fail2ban':
+      package_ensure => 'latest',
+      config_file_template => "fail2ban/${::lsbdistcodename}/etc/fail2ban/jail.conf.erb",
+      bantime => 3600,
+      require => Package['sendmail'],
+    }
+
   }
 
-}
+  if($::operatingsystem == "centos" and $::operatingsystemrelease >= 6 and $::operatingsystemrelease < 7) {
 
-if($::operatingsystem == "centos" and $::operatingsystemrelease >= 6 and $::operatingsystemrelease < 7) {
+    include ::remi
 
-  include ::remi
+    package { "epel-release":
+      ensure => "latest",
+    }
 
-  package { "epel-release":
-    ensure => "latest",
-  }
+    ini_setting { 'centos base repo exclude php packages':
+      ensure  => present,
+      path    => "/etc/yum.repos.d/CentOS-Base.repo",
+      section => 'base',
+      setting => 'exclude',
+      value   => "php-*",
+    }
 
-  ini_setting { 'centos base repo exclude php packages':
-    ensure  => present,
-    path    => "/etc/yum.repos.d/CentOS-Base.repo",
-    section => 'base',
-    setting => 'exclude',
-    value   => "php-*",
-  }
+    ini_setting { 'centos base repo exclude php packages2':
+      ensure  => present,
+      path    => "/etc/yum.repos.d/CentOS-Base.repo",
+      section => 'updates',
+      setting => 'exclude',
+      value   => "php-*",
+      require => Ini_setting["centos base repo exclude php packages"],
+    }
 
-  ini_setting { 'centos base repo exclude php packages2':
-    ensure  => present,
-    path    => "/etc/yum.repos.d/CentOS-Base.repo",
-    section => 'updates',
-    setting => 'exclude',
-    value   => "php-*",
-    require => Ini_setting["centos base repo exclude php packages"],
-  }
+    if ! defined(Yumrepo['remi-php56']){
 
-  if ! defined(Yumrepo['remi-php56']){
-
-    yumrepo { 'remi-php56':
+      yumrepo { 'remi-php56':
             name       => 'remi-php56',
             descr      => 'Les RPM de remi de PHP 5.6 pour Enterprise Linux 6 - $basearch',
             baseurl    => absent,
@@ -61,14 +64,16 @@ if($::operatingsystem == "centos" and $::operatingsystemrelease >= 6 and $::oper
             enabled    => 1,
             before     => Class["owncloud"],
             require    => [ Class['::remi'], Ini_setting["centos base repo exclude php packages2"], ]
+      }
+
     }
 
   }
 
-}
-
-package{ 'office package':
+  package{ 'office package':
     name => 'libreoffice',
     ensure => latest,
+  }
+
 }
 
