ec3f6b3527eed7da4ab84991daaacc75e6d5b60e
Patrik Majer <patrik.majer@atlas.cz>
2016-08-31 20:49:20 +0200

    add config for mysql-server

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,8 +11,22 @@
 class owncloudstack (
 $owncloud_version="8",
 $manage_apache=true,
+$mysql_override_options = {},
 )
 {
+
+  $mysql_override_options_profile = { 'mysqld' => { 
+      'bind-address' => '127.0.0.1',
+            'log_warnings' => '4',
+            'slow_query_log' => '1',
+            'slow_query_log_file' => '/var/log/mysql-slow.log',
+            'log_output' => 'table,file', #!! for GENERAL and SLOW-QUERY
+            'long_query_time' => '3',
+            'query_cache_type' => '1',
+            'query_cache_size' => '64M',
+  } }
+
+  $mysql_override_options = deep_merge($mysql_override_options, $mysql_override_options_profile)
 
   class { 'owncloudstack::system':
   }
@@ -27,6 +41,27 @@ $manage_apache=true,
     restart  => 'true',
     require => Package["mysql-repo"],
   }
+
+    # slow query log
+    file { "mysql-server slow query log":
+      ensure  => present,
+      path    => "/var/log/mysql-slow.log",
+      owner   => 'mysql',
+      group   => 'mysql',
+      mode    => '0640',
+      notify => Service["mysqld"],
+      require => Package["mysql-community-server"],
+    }
+
+    # logrotate for mysql slow-query log
+    file { "mysql-server slow query log logrotate":
+      ensure  => present,
+      path    => "/etc/logrotate.d/mysql-slow",
+      content => template('owncloudstack/logrotate.conf.mysql-slow.erb'),
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+    }
 
   class { 'sendmail': 
   }
