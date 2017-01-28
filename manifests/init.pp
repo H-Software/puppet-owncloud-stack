7c5793da7f8c6d96c98ab622709f0a1d4f2c1c2f
Patrik (czhujer) Majer <patrik.majer@atlas.cz>
2017-01-28 18:02:10 +0100

    fix puppet lint error

--- manifests/init.pp
+++ manifests/init.pp
@@ -52,12 +52,12 @@ $mysql_override_options = {},
 
     # slow query log
     file { 'mysql-server slow query log':
-      ensure  => present,
-      path    => '/var/log/mysql-slow.log',
-      owner   => 'mysql',
-      group   => 'mysql',
-      mode    => '0640',
-      notify  => Service['mysqld'],
+      ensure   => present,
+      path     => '/var/log/mysql-slow.log',
+      owner    => 'mysql',
+      group    => 'mysql',
+      mode     => '0640',
+      notify   => Service['mysqld'],
       #require => Class['::mysql::server::service'],
     }
 
