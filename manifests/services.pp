03a51aa92524b2891d14a697e6ee927cf3ca99c0
Patrik (czhujer) Majer <patrik.majer@atlas.cz>
2017-01-28 09:14:58 +0100

    fix puppet linting

--- manifests/services.pp
+++ manifests/services.pp
@@ -15,13 +15,13 @@ class owncloudstack::services ()
   # Antivirus part
   #
 
-  if($::operatingsystem == "centos" and $::operatingsystemrelease >= 6 and $::operatingsystemrelease < 7) {
-    $packages_clamav = ["clamav", "clamd", "clamav-db"]
-    $service_clamav = "clamd"
+  if($::operatingsystem == 'centos' and $::operatingsystemrelease >= 6 and $::operatingsystemrelease < 7) {
+    $packages_clamav = ['clamav', 'clamd', 'clamav-db']
+    $service_clamav = 'clamd'
   }
   else{
-    $packages_clamav = ["clamav", "clamav-base", "clamav-daemon", "clamav-freshclam"]
-    $service_clamav = "clamav-freshclam"
+    $packages_clamav = ['clamav', 'clamav-base', 'clamav-daemon', 'clamav-freshclam']
+    $service_clamav = 'clamav-freshclam'
   }
 
   if($::owncloudstack::manage_clamav){
@@ -29,22 +29,22 @@ class owncloudstack::services ()
       ensure => latest,
     }
 
-    if($::operatingsystem == "ubuntu") {
+    if($::operatingsystem == 'ubuntu') {
 
       service { 'clamav-freshclam':
-        ensure => running,
-        enable => true,
-        require => Package[$packages_clamav],
+        ensure   => running,
+        enable   => true,
+        require  => Package[$packages_clamav],
       }
 
     }
 
     service { 'clamav-daemon':
-      name   => $service_clamav,
-      ensure => running,
-      enable => true,
-      require => Package[$packages_clamav],
-   }
+      name     => $service_clamav,
+      ensure   => running,
+      enable   => true,
+      require  => Package[$packages_clamav],
+    }
 
   }
 
@@ -52,14 +52,14 @@ class owncloudstack::services ()
   # Cron part
   #
 
-  if($::operatingsystem == "centos" and $::operatingsystemrelease >= 6 and $::operatingsystemrelease < 7) {
+  if($::operatingsystem == 'centos' and $::operatingsystemrelease >= 6 and $::operatingsystemrelease < 7) {
     $package_name = 'cronie'
     $service_name = 'crond'
     $apache_user  = 'apache'
   }
   else
   {
-    $package_name = 'cron' 
+    $package_name = 'cron'
     $service_name = 'cron'
     $apache_user  = 'www-run'
   }
@@ -70,19 +70,19 @@ class owncloudstack::services ()
   }
 
   service {'cron':
-    enable      => "true",                     # true (start on boot)
-    ensure      => "true",                     # true (running), false (stopped)
+    enable      => 'true',                     # true (start on boot)
+    ensure      => 'true',                     # true (running), false (stopped)
     name        => $service_name,
     require     => Package['cron'],
-    subscribe	=> File['owncloud cron file'],
+    subscribe   => File['owncloud cron file'],
   }
 
   $cron_file_owncloud = "*/15  *  *  *  * ${apache_user} php -f /var/www/owncloud/cron.php > /dev/null 2>&1\n"
 
   file { 'owncloud cron file':
-    name => '/etc/cron.d/owncloud',
-    content => $cron_file_owncloud,
-    require => Package["cron"],
+    name      => '/etc/cron.d/owncloud',
+    content   => $cron_file_owncloud,
+    require   => Package['cron'],
   }
 
 }
