a9e896eb613b4498882186eaff9f86a3a0462e75
Patrik (czhujer) Majer <patrik.majer@atlas.cz>
2017-01-29 19:18:55 +0100

    fix facts for unit tests

--- spec/classes/main_spec.rb
+++ spec/classes/main_spec.rb
@@ -72,6 +72,7 @@ describe 'owncloudstack' do
          :osfamily                  => 'RedHat',
          :operatingsystem           => 'RedHat',
          :operatingsystemmajrelease => '6',
+         :operatingsystemrelease    => '6.8',
          :architecture              => 'x86_64',
        }}
 
