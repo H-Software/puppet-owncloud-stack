c34506911cb7eb467365e6dbb8084e944b956f6b
Patrik (czhujer) Majer <patrik.majer@atlas.cz>
2017-10-25 22:33:58 +0200

    improve basic test suite

--- spec/classes/main_spec.rb
+++ spec/classes/main_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'owncloudstack' do
+describe 'sugarcrmstack_ng' do
   context 'supported operating systems' do
     on_supported_os.each do |os, facts|
       context "on #{os}" do
@@ -8,53 +8,116 @@ describe 'owncloudstack' do
           facts
         end
 
-        context "owncloudstack class without any parameters" do
+        #fixes for composer
+        let(:facts) { facts.merge( { 'composer_home' => '~', 'execs' => {} } ) }
+
+        context "sugarcrmstack_ng class without any parameters" do
           it { is_expected.to compile.with_all_deps }
 
-          it { is_expected.to contain_class('owncloudstack') }
-          it { is_expected.to contain_class('owncloudstack::system') }
-          it { is_expected.to contain_class('owncloudstack::services') }
-          it { is_expected.to contain_class('owncloudstack::mysql') }
-          it { is_expected.to contain_class('owncloudstack::owncloud') }
-          it { is_expected.to contain_class('owncloudstack::system').that_comes_before('owncloudstack::services') }
-          it { is_expected.to contain_class('owncloudstack::services').that_comes_before('owncloudstack::mysql') }
-          it { is_expected.to contain_class('owncloudstack::mysql').that_comes_before('owncloudstack::owncloud') }
+          it { is_expected.to contain_class('sugarcrmstack_ng') }
+          it { is_expected.to contain_class('sugarcrmstack_ng::params') }
+          it { is_expected.to contain_class('sugarcrmstack_ng::install') }
+#          it { is_expected.to contain_class('sugarcrmstack_ng::install').that_comes_before('sugarcrmstack_ng::config') }
+          it { is_expected.to contain_class('sugarcrmstack_ng::config') }
+          it { is_expected.to contain_class('sugarcrmstack_ng::service') }
+#          it { is_expected.to contain_class('sugarcrmstack_ng::service').that_subscribes_to('sugarcrmstack_ng::config') }
 
-#          it { is_expected.to contain_package('owncloud').with_ensure('installed') }
-#          it { is_expected.to contain_package('php').with_ensure('installed') }
-#          it { is_expected.to contain_package('php-ldap').with_ensure('installed') }
-#          it { is_expected.to contain_package('php-mysqlnd').with_ensure('installed') }
-          it { is_expected.to contain_package('fail2ban').with_ensure('latest') }
-#          it { is_expected.to contain_package('libreoffice').with_ensure('latest') }
+          it { is_expected.to contain_class('sugarcrmstack_ng::apache_php') }
 
-#          it { is_expected.to contain_service('mysqld').with(
-#                'ensure'     => 'running',
-#                'enable'     => 'true',
-#                'hasrestart' => 'true',
-#               ) }
+          it { should_not contain_class('sugarcrmstack_ng::mysql_server') }
 
-#          it { is_expected.to contain_service('sendmail').with(
-#                'ensure'     => 'running',
-#                'enable'     => 'true',
-#                'hasrestart' => 'true',
-#               ) }
+          # generic part
+          ['apachetop', 'bind-utils', 'htop', 'iftop',
+           'iotop', 'iptraf', 'irqbalance', 'links', 'lsof', 'lsscsi', 'lynx',
+           'nano', 'numad', 'policycoreutils-python', 'sysstat', 'telnet', 'traceroute',
+           'unzip', 'vim-enhanced', 'vim-minimal', 'wget', 'yum-cron', 'yum-utils', 'zip',
+           ].each do |x| it {
+             is_expected.to contain_package(x)
+               .with(ensure: 'installed')
+             }
+          end
 
-          it { is_expected.to contain_service('fail2ban').with(
-                'ensure'     => 'running',
-                'enable'     => 'true',
-                'hasrestart' => 'true',
-               ) }
+          # only CentOS 6 packages
+          case facts[:osfamily]
+          when 'RedHat'
+            case facts[:operatingsystemmajrelease]
+              when '6'
+                it { should contain_package("hal").with(ensure: 'installed') }
+                it { should contain_package("cpuspeed").with(ensure: 'installed') }
+              else
+                it { should_not contain_package("hal").with(ensure: 'installed') }
+                it { should_not contain_package("cpuspeed").with(ensure: 'installed') }
+              end
+          end
 
+          # apache+php part
+          it { is_expected.to contain_class("apache::params") }
+          it { is_expected.to contain_package("httpd").with(
+              'notify' => 'Class[Apache::Service]',
+              'ensure' => 'installed'
+            )
+          }
 
-
+          # NOT EXISTS mysql_server part
 
         end
+
+        context "sugarcrmstack_ng class without apache_php" do
+          # switch param
+          let(:params) { {'apache_php_enable' => false} }
+
+          # check compile
+          it { is_expected.to compile.with_all_deps }
+
+          it { is_expected.to contain_class('sugarcrmstack_ng') }
+          it { is_expected.to contain_class('sugarcrmstack_ng::params') }
+          it { is_expected.to contain_class('sugarcrmstack_ng::install') }
+#          it { is_expected.to contain_class('sugarcrmstack_ng::install').that_comes_before('sugarcrmstack_ng::config') }
+          it { is_expected.to contain_class('sugarcrmstack_ng::config') }
+          it { is_expected.to contain_class('sugarcrmstack_ng::service') }
+#          it { is_expected.to contain_class('sugarcrmstack_ng::service').that_subscribes_to('sugarcrmstack_ng::config') }
+
+          it { should_not contain_class('sugarcrmstack_ng::apache_php') }
+          it { should_not contain_class('sugarcrmstack_ng::mysql_server') }
+
+          # generic part
+          ['apachetop', 'bind-utils', 'htop', 'iftop',
+           'iotop', 'iptraf', 'irqbalance', 'links', 'lsof', 'lsscsi', 'lynx',
+           'nano', 'numad', 'policycoreutils-python', 'sysstat', 'telnet', 'traceroute',
+           'unzip', 'vim-enhanced', 'vim-minimal', 'wget', 'yum-cron', 'yum-utils', 'zip',
+           ].each do |x| it {
+             is_expected.to contain_package(x)
+               .with(ensure: 'installed')
+             }
+          end
+
+          # only CentOS 6 packages
+          case facts[:osfamily]
+          when 'RedHat'
+            case facts[:operatingsystemmajrelease]
+              when '6'
+                it { should contain_package("hal").with(ensure: 'installed') }
+                it { should contain_package("cpuspeed").with(ensure: 'installed') }
+              else
+                it { should_not contain_package("hal").with(ensure: 'installed') }
+                it { should_not contain_package("cpuspeed").with(ensure: 'installed') }
+              end
+          end
+
+          # NOT EXISTS apache+php part
+          it { should_not contain_class("apache::params") }
+          it { should_not contain_package("httpd") }
+
+          # NOT EXISTS mysql_server part
+
+        end
+
       end
     end
   end
 
   context 'unsupported operating system' do
-    describe 'owncloudstack class without any parameters on Solaris/Nexenta' do
+    describe 'sugarcrmstack_ng class without any parameters on Solaris/Nexenta' do
       let(:facts) do
         {
           :osfamily        => 'Solaris',
@@ -62,176 +125,7 @@ describe 'owncloudstack' do
         }
       end
 
-      it { expect { is_expected.to contain_package('owncloudstack') }.to raise_error(Puppet::Error, /Solaris not supported/) }
+      it { expect { is_expected.to contain_package('sugarcrmstack_ng') }.to raise_error(Puppet::Error, /Nexenta not supported/) }
     end
   end
-
-  context 'RedHat/CentOS 6 operaton system' do
-    describe 'owncloudstack class without any parameters on RedHat/CentOS 6' do
-       let (:facts) {{
-         :osfamily                  => 'RedHat',
-         :operatingsystem           => 'CentOS',
-         :operatingsystemmajrelease => '6',
-         :operatingsystemrelease    => '6.8',
-         :architecture              => 'x86_64',
-         :ipaddress                 => '10.42.42.42',
-         :is_virtual                => true
-       }}
-
-#         it { is_expected.to contain_package('mysql-community-server').with_ensure('installed') }
-         it { is_expected.to contain_package('httpd').with_ensure('installed') }
-         it { is_expected.to contain_package('sendmail').with_ensure('present') }
-
-         it { is_expected.to contain_service('clamav-daemon').with(
-                'ensure'     => 'running',
-                'enable'     => 'true',
-               ) }
-
-#         it { is_expected.to contain_service('ntpd').with(
-#                'ensure'     => 'running',
-#                'enable'     => 'true',
-#                'hasrestart' => 'true',
-#               ) }
-
-         it { is_expected.to contain_service('cron').with(
-                'ensure'     => 'true',
-                'enable'     => 'true',
-                #'hasrestart' => 'true',
-               ) }
-
-
-         it { is_expected.to contain_service('httpd').with(
-                'ensure'     => 'running',
-                'enable'     => 'true',
-                'hasrestart' => 'true',
-               ) }
-
-         it { is_expected.to contain_file('/var/www/html/owncloud').with({
-              'ensure' => 'directory',
-              'owner'  => 'root',
-              #'mode'   => '0755',
-              'group'  => 'root',
-            })
-          }
-
-    end
-  end
-
-  context 'Debian operation system' do
-    describe 'owncloudstack class without any parameters on Debian' do
-       let (:facts) {{
-         :osfamily                  => 'Debian',
-         :lsbdistid                 => 'Debian',
-         :operatingsystem           => 'Debian',
-         :operatingsystemmajrelease => '',
-         :operatingsystemrelease    => '7.11',
-         :lsbdistcodename           => 'wheezy',
-         :ipaddress                 => '10.42.42.42',
-         :is_virtual                => true
-       }}
-       it {
-
-         #is_expected.to contain_package('apache2.2-bin').with_ensure('installed')
-         #is_expected.to contain_package('apache2-utils').with_ensure('installed')
-       }
-
-       #it { is_expected.to contain_package('sendmail-bin').with_ensure('installed') }
-
-       it { is_expected.to contain_service('clamav-daemon').with(
-                'ensure'     => 'running',
-                'enable'     => 'true',
-               ) }
-
-       it { is_expected.to contain_service('clamav-freshclam').with(
-                'ensure'     => 'running',
-                'enable'     => 'true',
-               ) }
-
-       it { is_expected.to contain_service('ntp').with(
-                'ensure'     => 'running',
-                'enable'     => 'true',
-                'hasrestart' => 'true',
-               ) }
-
-       it { is_expected.to contain_service('cron').with(
-                'ensure'     => 'true',
-                'enable'     => 'true',
-               ) }
-
-
-#       it { is_expected.to contain_service('apache2').with(
-#                'ensure'     => 'running',
-#                'enable'     => 'true',
-#                'hasrestart' => 'true',
-#               ) }
-
-       it { is_expected.to contain_file('/var/www/owncloud').with({
-              'ensure' => 'directory',
-              'owner'  => 'root',
-              #'mode'   => '0755',
-              'group'  => 'root',
-            })
-          }
-
-    end
-  end
-
-  context 'Ubuntu operation system' do
-    describe 'owncloudstack class without any parameters on Ubuntu' do
-       let (:facts) {{
-         :osfamily                  => 'Debian',
-         :lsbdistid                 => 'Ubuntu',
-         :operatingsystem           => 'Ubuntu',
-         :operatingsystemmajrelease => '12.04',
-         :operatingsystemrelease    => '12.04',
-         :lsbdistcodename           => 'precise',
-         :lsbdistrelease            => '12.04',
-         :ipaddress                 => '10.42.42.42',
-         :is_virtual                => true
-       }}
-       it {
-         #is_expected.to contain_package('apache2.2-bin').with_ensure('installed')
-         #is_expected.to contain_package('apache2-utils').with_ensure('installed')
-       }
-       it { is_expected.to contain_package('sendmail-bin').with_ensure('present') }
-
-       it { is_expected.to contain_service('clamav-daemon').with(
-                'ensure'     => 'running',
-                'enable'     => 'true',
-               ) }
-
-       it { is_expected.to contain_service('clamav-freshclam').with(
-                'ensure'     => 'running',
-                'enable'     => 'true',
-               ) }
-
-       it { is_expected.to contain_service('ntp').with(
-                'ensure'     => 'running',
-                'enable'     => 'true',
-                'hasrestart' => 'true',
-               ) }
-
-       it { is_expected.to contain_service('cron').with(
-                'ensure'     => 'true',
-                'enable'     => 'true',
-               ) }
-
- 
-#       it { is_expected.to contain_service('apache2').with(
-#                'ensure'     => 'running',
-#                'enable'     => 'true',
-#                'hasrestart' => 'true',
-#               ) }
-
-       it { is_expected.to contain_file('/var/www/owncloud').with({
-              'ensure' => 'directory',
-              'owner'  => 'root',
-              #'mode'   => '0755',
-              'group'  => 'root',
-            })
-          }
-    end
-  end
-
 end
-
