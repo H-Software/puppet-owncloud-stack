ad646ae8874c9f2befa40c152e5571149ac2556f
Patrik (czhujer) Majer <patrik.majer@atlas.cz>
2018-01-24 12:05:22 +0100

    fix vars for mysql pkg name

--- manifests/init.pp
+++ manifests/init.pp
@@ -50,12 +50,12 @@ $libreoffice_pkg_name="libreoffice",
       }
       $require_mysql_server = Package['mysql-repo']
       $documentroot = '/var/www/html/owncloud'
-      $::owncloudstack::mysql_server_package = 'mysql-community-server'
+      $mysql_server_package = 'mysql-community-server'
   }
   elsif ($::operatingsystem == 'ubuntu' or $::operatingsystem == 'debian'){
     $require_mysql_server = []
     $documentroot = '/var/www/owncloud'
-    $::owncloudstack::mysql_server_package = 'mysql-server'
+    $mysql_server_package = 'mysql-server'
   }
   else{
     fail("${::osfamily} not supported")
